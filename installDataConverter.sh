ff7bf5d74455c61b6d2ba5edc41e3cb3815941be
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-04-21 11:06:27 -0700

    Make DataConverter a submodule

--- installDataConverter.sh
+++ installDataConverter.sh
@@ -3,14 +3,10 @@ set -euo pipefail
 
 projectRoot=$(pwd)
 
-rm -rf work || true
-mkdir ./work
-cd ./work
+git submodule update --init --recursive
 
-git clone https://github.com/PaperMC/DataConverter.git -b dev/1.20.5
 cd ./DataConverter
-./gradlew build --no-daemon
+chmod +x gradlew
+./gradlew clean build --no-daemon
 cp build/devlibs/*-dev.jar $projectRoot/dataconverter.jar
-
-cd "$projectRoot"
-rm -rf ./work
+git reset --hard
