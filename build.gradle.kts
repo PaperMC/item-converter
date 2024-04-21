bd8a0f405a90df2312e50e9c21cc54b4f586ae52
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-04-20 19:05:47 -0700

    Switch to VanillaGradle, update JDK back to 21, use CommandArgumentUpgrader from DataConverter

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
-    id("io.papermc.paperweight.userdev") version "1.5.15"
+    id("org.spongepowered.gradle.vanilla") version "0.2.1-SNAPSHOT"
 }
 
 group = "io.papermc"
@@ -10,28 +10,31 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
 tasks.withType<JavaCompile>().configureEach {
-    options.release = 17
+    options.release = 21
 }
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+minecraft {
+    version("1.20.5-rc2")
 }
 
 dependencies {
-    paperweight.paperDevBundle("1.20.4-R0.1-SNAPSHOT")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation(files("dataconverter.jar"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-configurations.runtimeOnly {
-    extendsFrom(configurations.mojangMappedServerRuntime.get())
-}
 configurations.configureEach {
     exclude(module = "log4j-slf4j2-impl")
     exclude(group = "org.apache.logging.log4j")
+    exclude(group = "commons-logging", module = "commons-logging")
 }
--- src/main/java/io/papermc/converter/service/MinecraftService.java
+++ src/main/java/io/papermc/converter/service/MinecraftService.java
@@ -1,5 +1,6 @@
 package io.papermc.converter.service;
 
+import ca.spottedleaf.dataconverter.util.CommandArgumentUpgrader;
 import net.minecraft.SharedConstants;
 import net.minecraft.server.Bootstrap;
 import org.slf4j.Logger;
@@ -10,14 +11,25 @@ import org.springframework.stereotype.Service;
 public final class MinecraftService {
 	private static final Logger LOGGER = LoggerFactory.getLogger(MinecraftService.class);
 
+	private final CommandArgumentUpgrader upgrader;
+
 	public MinecraftService() {
 		LOGGER.info("Bootstrapping Minecraft...");
 		SharedConstants.tryDetectVersion();
 		Bootstrap.bootStrap();
 		LOGGER.info("Minecraft bootstrapped!");
+
+		LOGGER.info("Creating CommandArgumentUpgrader...");
+		this.upgrader = CommandArgumentUpgrader.upgrader_1_20_4_to_1_20_5(2);
+		LOGGER.info("CommandArgumentUpgrader created.");
 	}
 
 	public String upgradeCommand(final String input) {
-		return "Soon:tm: - " + input; // TODO
+		LOGGER.debug("Upgrading command '{}'", input);
+		// We do a startsWith check because we aren't supporting WorldEdit style commands
+		// and want to support leading '/' or no leading '/'
+		final String upgraded = this.upgrader.upgradeCommandArguments(input, input.startsWith("/"));
+		LOGGER.debug("Upgraded command '{}' -> '{}'", input, upgraded);
+		return upgraded;
 	}
 }
